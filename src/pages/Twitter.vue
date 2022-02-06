bef8f6c5817a527a6096b55b9b1a6d4fac0167b6
Vivi <vachetvirginie@gmail.com>
2022-02-06 18:15:08 +0100

    add tailwind

---
+++ src/pages/Twitter.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <div
+      class="container mx-auto flex px-5 py-24 items-center justify-center flex-col"
+    >
+      <a
+        class="twitter-timeline"
+        href="https://twitter.com/VirginieVachet?ref_src=twsrc%5Etfw"
+        >Tweets by VirginieVachet</a
+      >
+    </div>
+  </q-page>
+</template>
+<script>
+export default {
+  name: "PageTwitter",
+};
+</script>
